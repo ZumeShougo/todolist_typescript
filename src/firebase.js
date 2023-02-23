3790e09ee35ff47f7059234ec837152d927337d0
ZumeShougo <g0109725rena@gmail.com>
2023-02-23 09:25:17 +0900

    install typscript

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPLLUXSk-k-LkTgJzqj_8QXOJSxifM4r4",
+  authDomain: "todolisttypescript.firebaseapp.com",
+  projectId: "todolisttypescript",
+  storageBucket: "todolisttypescript.appspot.com",
+  messagingSenderId: "109464713425",
+  appId: "1:109464713425:web:a52b4ad0bfe14e06e7edf0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
